4cde860a7bb11b6eeb1392d45672296c9f8761ab
Beatriz Viana <beavianasp@gmail.com>
2019-03-24 18:33:42 -0300

    added project files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 atual, nasc, sub, rd;
+            Console.WriteLine("Digite o ano atual.");
+            atual = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o seu ano de nascimento.");
+            nasc = Convert.ToInt32(Console.ReadLine());
+            sub = (atual - nasc);
+            rd = (2050 -nasc);
+            Console.WriteLine("Você tem "+ sub+ " anos.");
+            Console.WriteLine("Você terá " + rd + " anos em 2050");
+            Console.ReadKey();
+        }
+    }
+}
